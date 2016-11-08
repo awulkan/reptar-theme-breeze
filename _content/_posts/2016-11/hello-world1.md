ddff77e593ff6bcb72b42537cf4ae40c0e269f0a
AWulkan <alexanderwulkan@hotmail.com>
2016-11-08 23:08:11 +0100

    Theme foundation

--- _content/_posts/2016-11/hello-world1.md
+++
@@ -1,9 +0,0 @@
----
-title: Hello World!
-date: 2016-11-1
-tags:
-- meta
-- test
----
-
-Welcome to my site!
--- _themes/breeze/js/main.js
+++ _themes/breeze/js/main.js
@@ -1,1 +1,11 @@
-console.log('Hello World');
+(function() {
+    "use strict";
+
+    var topNav = document.getElementById("topnav-js");
+    var hamburger = document.getElementById("hamburger");
+    hamburger.addEventListener("click", myFunc);
+
+    function myFunc(e) {
+        topNav.classList.toggle("responsive");
+    }
+}());
