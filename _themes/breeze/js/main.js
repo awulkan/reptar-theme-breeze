87fd5b38b34556a7656467800b87887626b4aab1
AWulkan <alexanderwulkan@hotmail.com>
2016-11-10 23:12:40 +0100

    Created Mobile Menu

--- _themes/breeze/js/main.js
+++ _themes/breeze/js/main.js
@@ -1,11 +1,42 @@
+// (function() {
+//     "use strict";
+//
+//     var topNav = document.getElementById("topnav-js");
+//     var hamburger = document.getElementById("hamburger");
+//     hamburger.addEventListener("click", myFunc);
+//
+//     function myFunc(e) {
+//         topNav.classList.toggle("responsive");
+//     }
+// }());
+
+
 (function() {
     "use strict";
 
     var topNav = document.getElementById("topnav-js");
     var hamburger = document.getElementById("hamburger");
-    hamburger.addEventListener("click", myFunc);
+    hamburger.addEventListener("click", toggleMenu);
 
-    function myFunc(e) {
-        topNav.classList.toggle("responsive");
+    function toggleMenu() {
+        var menuState = topNav.style.display;
+        if(menuState === "flex") {
+            topNav.style.display = "none";
+        } else {
+            topNav.style.display = "flex";
+        }
     }
 }());
+
+
+// (function() {
+//   var $body = document.body,
+//     $menu_trigger = $body.getElementsByClassName('menu-trigger')[0];
+//
+//   if (typeof $menu_trigger !== 'undefined') {
+//     $menu_trigger.addEventListener('click', function() {
+//       $body.className = ($body.className == 'menu-active') ? '' : 'menu-active';
+//     });
+//   }
+//
+// }).call(this);
